09f95860d616a5ae2bc52ab708eeb5d2e127a81a
Muhammad Zaid <139756201+Zaid0303@users.noreply.github.com>
2024-09-19 02:03:38 +0500

    Addi Login & SignUp Page

--- e-Ambulance/js/main.js
+++ e-Ambulance/js/main.js
@@ -1,8 +1,39 @@
+// Loader Function
+function hideLoader() {
+    setTimeout(function () {
+        document.getElementById('loader').style.display = 'none';
+        document.getElementById('content').style.display = 'block';
+    }, 2000); // Loader will be visible for 2 seconds
+}
+
+////////////// Navbar \\\\\\\\\\\\\\\\\
 // Scroll to sections on nav click
-$('a[href*="#"]').on('click', function(e) {
+$('a[href*="#"]').on('click', function (e) {
     e.preventDefault();
 
     $('html, body').animate({
         scrollTop: $($(this).attr('href')).offset().top - 100
     }, 500);
 });
+////////////// Navbar \\\\\\\\\\\\\\\\\
+
+
+////////////// Login & SignUp \\\\\\\\\\\\\\\\\
+// Function to toggle eye icon password visibility
+function togglePassword(inputId, iconId) {
+    const passwordField = document.getElementById(inputId);
+    const icon = document.getElementById(iconId);
+
+    if (passwordField.type === "password") {
+        passwordField.type = "text";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+    } else {
+        passwordField.type = "password";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+    }
+}
+////////////// Login & SignUp \\\\\\\\\\\\\\\\\
+
+
